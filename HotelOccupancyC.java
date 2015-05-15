fc55a9e5bd2c81b671981c5175e8f5f799f7ee50
matosrob <matos_r_@hotmail.com>
2015-05-14 21:26:56 -0400

    Hotel Occupancy
    Determines the occupancy percentage of a hotel

---
+++ HotelOccupancyC.java
@@ -0,0 +1,74 @@
+package week_5;
+
+
+/**
+ *
+ * @author Roberto
+ */
+public class HotelOccupancyC 
+{
+    int floors;          // Number of floors
+    int totalRooms;      // Total number of rooms
+    int rooms;           //Rooms on a floor
+    private int totalOccupied;   // Total rooms occupied
+    private int occupied;        // Occupied rooms on a floor
+    private int vacantRooms;     // Number of vacant rooms
+    double occupancy;    // Occupancy rate
+    
+    public void setFloors(int floors)
+    {
+        this.floors = floors;
+    }
+    public void setTotalRooms(int totalRooms)
+    {
+        this.totalRooms = totalRooms;
+    }
+    public void setRooms(int rooms)
+    {
+        this.rooms = rooms;
+    }
+    public void setTotalOccupied(int totalOccupied)
+    {
+        this.totalOccupied = totalOccupied;
+    }
+    public void setOccupied(int occupied)
+    {
+        this.occupied = occupied;
+    }
+    public void setVacantRooms(int vacantRooms)
+    {
+        this.vacantRooms = vacantRooms;
+    }
+    public void setOccupancy(double occupancy)
+    {
+        this.occupancy = occupancy;
+    }
+    public int getFloors()
+    {
+        return floors;
+    }
+    public int getTotalRooms()
+    {
+        return totalRooms;
+    }
+    public int getRooms()
+    {
+        return rooms;
+    }
+    public int getTotalOccupied()
+    {
+        return totalOccupied;
+    }
+    public int getOccupied()
+    {
+        return occupied;
+    }
+    public int getVacantRooms()
+    {
+        return vacantRooms;
+    }
+    public double getOccupancy()
+    {
+        return occupancy;
+    }
+}
---
+++ HotelOccupancyD.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roberto
+ */
+public class HotelOccupancyD 
+
+{//Begin class
+
+    public static void main(String[] args)
+    {//Begin main method
+
+			
+    Scanner input = new Scanner(System.in);
+    HotelOccupancyC hotel = new HotelOccupancyC();
+
+
+
+    int floors = 0;
+    int totalOccupied = 0;
+    int totalRooms = 0;
+
+    System.out.print("How many floors does the hotel have?  ");
+    floors = input.nextInt();
+
+    while (floors < 1)
+    {
+        System.out.print("You must enter 1 or more: ");
+        floors = input.nextInt();
+    }
+    
+    hotel.setFloors(floors);
+
+    for (int i = 1; i <= floors; i++)
+    {
+        System.out.print("How many rooms does floor " + i + " have? ");
+        int rooms = input.nextInt();
+
+        while (rooms < 10)
+        {
+            System.out.print("You must enter 10 or more: ");
+            rooms = input.nextInt();
+        }
+
+        hotel.setRooms(rooms);
+    
+        totalRooms += rooms;
+        hotel.setTotalRooms(totalRooms);
+        System.out.print("How many occupied rooms does floor " + i  + " have? ");
+        int occupied = input.nextInt();
+
+        while (occupied > rooms)
+        {
+            System.out.print("You must enter " + rooms + " or less. Re-enter: ");
+            occupied = input.nextInt();
+        }
+
+         totalOccupied += occupied;
+         hotel.setTotalOccupied(totalOccupied);
+    }
+
+      // Calculate the number of vacant rooms.
+    hotel.setVacantRooms(hotel.getTotalRooms() - hotel.getTotalOccupied());
+
+      // Calculate the occupancy rate.
+    hotel.setOccupancy((double)hotel.getTotalOccupied() / hotel.getTotalRooms()); 
+
+    System.out.println("Number of rooms: " + hotel.getTotalRooms());
+    System.out.println("Occupied rooms: " + hotel.getTotalOccupied());
+    System.out.println("Vacant rooms: " + hotel.getVacantRooms());
+    System.out.println("Occupancy rate: " + hotel.getOccupancy() * 100 + "%");
+    
+    
+   }
+}
+
